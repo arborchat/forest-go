204658a7a9c01eb1e511f5fb35ef123de0af1eb7
Chris Waldon <christopher.waldon.dev@gmail.com>
2019-11-21 14:54:47 -0500

    Document Add() behavior in Store interface

--- store.go
+++ store.go
@@ -16,6 +16,8 @@ type Store interface {
 	GetReply(communityID, conversationID, replyID *fields.QualifiedHash) (Node, bool, error)
 	Children(*fields.QualifiedHash) ([]*fields.QualifiedHash, error)
 	Recent(nodeType fields.NodeType, quantity int) ([]Node, error)
+	// Add inserts a node into the store. It is *not* an error to insert a node which is already
+	// stored. Implementations must not return an error in this case.
 	Add(Node) error
 }
 
